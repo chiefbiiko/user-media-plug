92e36aa941886513f75f51454fb7e64b4eeca763
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-09-06 13:34:50 +0200

    complete clientele

--- clientele/index.js
+++ clientele/index.js
@@ -12,18 +12,25 @@ const outbound = require('./lib/outbound.js')
 
 const debug = require('debug')('clientele')
 
+const ERR = {
+  CB_NOT_FUNC: TypeError('cb is not a function'),
+  RES_NOT_OK: Error('response status not ok'),
+  NOT_TRUTHY_STRING (x) { return TypeError(`${x} is not a truthy string`) },
+  NOT_STRING_ARRAY (x) { return TypeError(`${x} is not a string array`) }
+}
+
 function Clientele (url, user) { // url can just be 'ws://localhost:10000'
   if (!(this instanceof Clientele)) return new Clientele(url, user)
   EventEmitter.call(this)
 
   if (!/firefox/i.test(navigator.userAgent))
-    console.error(`clientele probly won't work on ${navigator.userAgent}`)
+    debug(`clientele probly won't work on ${navigator.userAgent}`)
   else if (!MediaSource.isTypeSupported(Clientele.MIME_CODEC) ||
            !MediaRecorder.isTypeSupported(Clientele.MIME))
     throw Error(`unsupported MIME type or codec: ${Clientele.MIME_CODEC}`)
 
-  if (!isTruthyString(url)) throw TypeError('url is not a truthy string')
-  if (!isTruthyString(user)) throw TypeError('user is not a truthy string')
+  if (!isTruthyString(url)) throw ERR.NOT_TRUTHY_STRING('url')
+  if (!isTruthyString(user)) throw ERR.NOT_TRUTHY_STRING('user')
 
   this._user = user
 
@@ -87,8 +94,7 @@ Clientele.prototype._makeVideoStream = function makeVideoStream (msg) {
 }
 
 Clientele.prototype.whoami = function whoami (cb) {
-  if (typeof cb !== 'function')
-    return cb(TypeError('cb is not a function'))
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
 
   const self = this
   const tx = Math.random()
@@ -97,7 +103,7 @@ Clientele.prototype.whoami = function whoami (cb) {
     if (err) return cb(err)
     self._metastream_valve
       .subscribe(
-        res => cb(res.ok ? null : Error('response status not ok')),
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
         res => res.tx === tx,
         1
       )
@@ -105,10 +111,8 @@ Clientele.prototype.whoami = function whoami (cb) {
 }
 
 Clientele.prototype.register = function register (password, cb) {
-  if (!isTruthyString(password))
-    return cb(TypeError('password is not a truthy string'))
-  if (typeof cb !== 'function')
-    return cb(TypeError('cb is not a function'))
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+  if (!isTruthyString(password)) return cb(ERR.NOT_TRUTHY_STRING('password'))
 
   const self = this
   const tx = Math.random()
@@ -117,7 +121,7 @@ Clientele.prototype.register = function register (password, cb) {
     if (err) return cb(err)
     self._metastream_valve
       .subscribe(
-        res => cb(res.ok ? null : Error('response status not ok')),
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
         res => res.tx === tx,
         1
       )
@@ -125,10 +129,8 @@ Clientele.prototype.register = function register (password, cb) {
 }
 
 Clientele.prototype.login = function login (password, cb) {
-  if (!isTruthyString(password))
-    return cb(TypeError('password is not a truthy string'))
-  if (typeof cb !== 'function')
-    return cb(TypeError('cb is not a function'))
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+  if (!isTruthyString(password)) return cb(ERR.NOT_TRUTHY_STRING('password'))
 
   const self = this
   const tx = Math.random()
@@ -137,7 +139,7 @@ Clientele.prototype.login = function login (password, cb) {
     if (err) return cb(err)
     self._metastream_valve
       .subscribe(
-        res => cb(res.ok ? null : Error('response status not ok')),
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
         res => res.tx === tx,
         1
       )
@@ -145,8 +147,7 @@ Clientele.prototype.login = function login (password, cb) {
 }
 
 Clientele.prototype.logout = function logout (cb) {
-  if (typeof cb !== 'function')
-    return cb(TypeError('cb is not a function'))
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
 
   const self = this
   const tx = Math.random()
@@ -155,24 +156,88 @@ Clientele.prototype.logout = function logout (cb) {
     if (err) return cb(err)
     self._metastream_valve
       .subscribe(
-        res => cb(res.ok ? null : Error('response status not ok')),
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
         res => res.tx === tx,
         1
       )
   })
 }
 
-Clientele.prototype.addPeers = function addPeers (peers, cb) {}
-Clientele.prototype.deletePeers = function deletePeers (peers, cb) {}
-Clientele.prototype.getPeers = function getPeers (cb) {}
-Clientele.prototype.status = function status (status, cb) {}
+Clientele.prototype.addPeers = function addPeers (peers, cb) {
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+  if (!isStringArray(peers)) return cb(ERR.NOT_STRING_ARRAY('peers'))
+
+  const self = this
+  const tx = Math.random()
+
+  self._metastream.write(outbound.addPeers(self._user, peers, tx), err => {
+    if (err) return cb(err)
+    self._metastream_valve
+      .subscribe(
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
+        res => res.tx === tx,
+        1
+      )
+  })
+}
+
+Clientele.prototype.deletePeers = function deletePeers (peers, cb) {
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+  if (!isStringArray(peers)) return cb(ERR.NOT_STRING_ARRAY('peers'))
+
+  const self = this
+  const tx = Math.random()
+
+  self._metastream.write(outbound.deletePeers(self._user, peers, tx), err => {
+    if (err) return cb(err)
+    self._metastream_valve
+      .subscribe(
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
+        res => res.tx === tx,
+        1
+      )
+  })
+}
+
+Clientele.prototype.getPeers = function getPeers (cb) {
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+
+  const self = this
+  const tx = Math.random()
+
+  self._metastream.write(outbound.getPeers(self._user, tx), err => {
+    if (err) return cb(err)
+    self._metastream_valve
+      .subscribe(
+        res => res.ok ? cb(null, res.peers) : cb(ERR.RES_NOT_OK),
+        res => res.tx === tx,
+        1
+      )
+  })
+}
+Clientele.prototype.status = function status (status, cb) {
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+  if (!isTruthyString(status)) return cb(ERR.NOT_TRUTHY_STRING('status'))
+
+  const self = this
+  const tx = Math.random()
+
+  self._metastream.write(outbound.status(self._user, status, tx), err => {
+    if (err) return cb(err)
+    self._metastream_valve
+      .subscribe(
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
+        res => res.tx === tx,
+        1
+      )
+  })
+}
 
 Clientele.prototype.call = function call (peer, cb) {
-  if (!isTruthyString(peer))
-    return cb(TypeError('peer is not a truthy string'))
-  if (typeof cb !== 'function')
-    return cb(TypeError('cb is not a function'))
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+  if (!isTruthyString(peer)) return cb(ERR.NOT_TRUTHY_STRING('peer'))
 
+  const self = this
   const tx = Math.random()
 
   self._metastream.write(outbound.call(self._user, peer, tx), err => {
@@ -182,7 +247,7 @@ Clientele.prototype.call = function call (peer, cb) {
         function listener (res) {
           if (!res.ok) {
             self._metastream_valve.unsubscribe(listener)
-            return cb(Error('response status not ok'), false)
+            return cb(ERR.RES_NOT_OK, false)
           }
           if (res.type === 'REJECT') cb(null, false)
           else if (res.type === 'ACCEPT') cb(null, true)
@@ -193,9 +258,59 @@ Clientele.prototype.call = function call (peer, cb) {
   })
 }
 
-Clientele.prototype.accept = function accept (peer, cb) {}
-Clientele.prototype.reject = function reject (peer, cb) {}
-Clientele.prototype.unpair = function unpair (peer, cb) {}
+Clientele.prototype.accept = function accept (peer, cb) {
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+  if (!isTruthyString(peer)) return cb(ERR.NOT_TRUTHY_STRING('peer'))
+
+  const self = this
+  const tx = Math.random()
+
+  self._metastream.write(outbound.accept(self._user, peer, tx), err => {
+    if (err) return cb(err)
+    self._metastream_valve
+      .subscribe(
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
+        res => res.tx === tx,
+        1
+      )
+  })
+}
+
+Clientele.prototype.reject = function reject (peer, cb) {
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+  if (!isTruthyString(peer)) return cb(ERR.NOT_TRUTHY_STRING('peer'))
+
+  const self = this
+  const tx = Math.random()
+
+  self._metastream.write(outbound.reject(self._user, peer, tx), err => {
+    if (err) return cb(err)
+    self._metastream_valve
+      .subscribe(
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
+        res => res.tx === tx,
+        1
+      )
+  })
+}
+
+Clientele.prototype.unpair = function unpair (peer, cb) {
+  if (typeof cb !== 'function') throw ERR.CB_NOT_FUNC
+  if (!isTruthyString(peer)) return cb(ERR.NOT_TRUTHY_STRING('peer'))
+
+  const self = this
+  const tx = Math.random()
+
+  self._metastream.write(outbound.accept(self._user, peer, tx), err => {
+    if (err) return cb(err)
+    self._metastream_valve
+      .subscribe(
+        res => cb(res.ok ? null : ERR.RES_NOT_OK),
+        res => res.tx === tx,
+        1
+      )
+  })
+}
 
 Clientele.prototype.__defineGetter__('user', function () { return this._user })
 
