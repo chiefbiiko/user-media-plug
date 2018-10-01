c17263c6047ca3fb02637a85e55946acf7000c65
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-10-01 15:13:54 +0200

    App.js owns the client

--- view/src/App.js
+++ view/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import store from './store'
+import { compose } from 'redux'
+// import store from './store'
+import configureStore from './store'
 import { SinglePage } from './components'
+import clientele from 'clientele/promised'
+
+const client = clientele('ws://localhost:10000')
+const store = configureStore(client)
+
+client.on('call', compose(store.dispatch, craftInboundCallAction))
+  .on('stop-ringing', compose(store.dispatch, craftInboundStopRingingAction))
+  .on('accept', compose(store.dispatch, craftInboundAcceptAction))
+  .on('reject', compose(store.dispatch, craftInboundRejectAction))
+  .on('unpair', compose(store.dispatch, craftInboundUnpairAction))
+  .on('status', compose(store.dispatch, craftPeerStatusAction))
+  .on('avatar', compose(store.dispatch, craftPeerAvatarAction))
+  .on('online', compose(store.dispatch, craftPeerOnlineAction))
+  .on('offline', compose(store.dispatch, craftPeerOfflineAction))
+  .on('videostream', video => { /* show da video element on ui */ })
 
 const ReduxApp = <Provider store={ store }><SinglePage /></Provider>
 
