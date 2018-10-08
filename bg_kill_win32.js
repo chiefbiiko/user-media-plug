7daeb5a7709a9b63e52a62a9ca4feef14543c03c
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-10-08 15:12:14 +0200

    better killer

--- bg_kill_win32.js
+++ bg_kill_win32.js
@@ -1,9 +1,10 @@
 const child = require('child_process')
 
 const line = child.execSync(
-  'wmic process where "name=\'node.exe\'" get processid,commandline | findstr /v findstr | findstr /c:/b'
+  'wmic process where "name=\'node.exe\'" get processid,commandline | ' +
+  'findstr /v findstr | findstr /c:"./index.js /b"'
 )
 
-const pid = parseInt(line.toString().replace(/[^\d]/g, ''))
+const pid = line.toString().replace(/^.*\s+(\d+)\s*$/, '$1')
 
 child.execSync(`taskkill /pid ${pid} /f`)
