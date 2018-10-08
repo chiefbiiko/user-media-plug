852c0fd87991082bd132efcd90a35c13cf926c34
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-10-08 13:41:45 +0200

    fully automated and self-cleansing windows tests

--- killer_win32.js
+++ killer_win32.js
@@ -1,7 +1,7 @@
 const child = require('child_process')
 
 const line = child.execSync(
-  'wmic process where "name=\'cmd.exe\'" get processid,commandline | findstr /v findstr | findstr /c:/b'
+  'wmic process where "name=\'node.exe\'" get processid,commandline | findstr /v findstr | findstr /c:/b'
 )
 
 const pid = parseInt(line.toString().replace(/[^\d]/g, ''))
