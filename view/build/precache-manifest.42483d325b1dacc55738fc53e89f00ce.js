a5c0352e53636db43ef1b1dc301637b038b2a811
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-10-08 13:57:09 +0200

    adds build dir

---
+++ view/build/precache-manifest.42483d325b1dacc55738fc53e89f00ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9aa06b46fe4d8e5c717c",
+    "url": "/static/js/main.9aa06b46.chunk.js"
+  },
+  {
+    "revision": "d05f363bc9bd2809c855",
+    "url": "/static/js/1.d05f363b.chunk.js"
+  },
+  {
+    "revision": "9aa06b46fe4d8e5c717c",
+    "url": "/static/css/main.ae3253f6.chunk.css"
+  },
+  {
+    "revision": "d05f363bc9bd2809c855",
+    "url": "/static/css/1.3f274c66.chunk.css"
+  },
+  {
+    "revision": "1aa99fc617220f90806d9ff78b973db6",
+    "url": "/index.html"
+  }
+];
