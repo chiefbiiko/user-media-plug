fd64232ee1b9f13f84fa435c8bc28104fc80ed44
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-10-09 13:53:30 +0200

    run prod-url with logs

--- prod_url.js
+++ prod_url.js
@@ -2,13 +2,15 @@ if (!process.env.NOW_URL) return
 
 const { readFile, writeFile } = require('fs')
 
+const fix = `wss://${process.env.NOW_URL}:${process.env.PORT}`
+
 readFile('./view/src/store/index.js', 'utf8', (err, txt) => {
   if (err) throw err
 
-  txt = txt.replace(
-    /ws:\/\/localhost:\d+/,
-    `wss://${process.env.NOW_URL}:41900`
-  )
+  txt = txt.replace(/ws:\/\/localhost:\d+/, fix)
 
-  writeFile('./view/src/store/index.js', txt, () => {})
+  writeFile('./view/src/store/index.js', txt, err => {
+    if (err) throw err
+    console.log(`fixed server address in ./view/src/store/index.js to: ${fix}`)
+  })
 })
